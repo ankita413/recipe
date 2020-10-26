bdaeba3b0b895ccc7d1195e2d184eca664f214ce
ankita413 <ankitamaurya413@gmail.com>
2020-10-26 14:11:03 +0530

    Updates

---
+++ precache-manifest.6f55a574cbc0db294f8b468ba775dc48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e33b2acb05c9844f8d953d19fda5c9e",
+    "url": "/recipe/index.html"
+  },
+  {
+    "revision": "ee08824bba7d623a6c5f",
+    "url": "/recipe/static/css/main.145c1ce9.chunk.css"
+  },
+  {
+    "revision": "afdb00122f9ccb899199",
+    "url": "/recipe/static/js/2.99ae3103.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/recipe/static/js/2.99ae3103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee08824bba7d623a6c5f",
+    "url": "/recipe/static/js/main.c3ec8c76.chunk.js"
+  },
+  {
+    "revision": "ee67d5ed58402f46e7bd",
+    "url": "/recipe/static/js/runtime-main.bcf1e2d0.js"
+  }
+]);
